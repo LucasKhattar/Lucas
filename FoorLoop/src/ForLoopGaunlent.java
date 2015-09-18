a1f66c0ac7164db8315c7322280a4ede5efcafdc
LucasKhattar <Guest@wts5.attlocal.net>
2015-09-17 20:00:39 -0700

    YAS!
    dood

---
+++ FoorLoop/src/ForLoopGaunlent.java
@@ -0,0 +1,63 @@
+import javax.naming.ldap.Rdn;
+
+
+public class ForLoopGaunlent {
+	
+//	Single For-Loops
+//	Write a for loop to do each the following:
+
+
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+			
+			}
+		}
+		for (int i = 0; i < args.length; i++) {
+			for (int j = 0; j < args.length; j++) {
+				
+			}
+		}
+		System.out.println("123");
+		System.out.println("456");
+		System.out.println("789");
+//		System.out.println("10");
+//		System.out.println("11");
+//		System.out.println("12");
+//		System.out.println("20");
+//		System.out.println("21");
+//		System.out.println("22");
+		//for (int i = 2004; i <=2015 ; i+=1) {
+//			int number = i*7;
+			//System.out.println(i + " Age "+(i-2004) );
+			//if (number==0){
+			//	System.out.print("Even ");
+			//}
+			//else{
+			
+			//}
+			
+			//System.out.println(i);
+		}
+
+
+//	2. Display the numbers 1 through 9 in a 3x3 square grid like this:
+//		1 2 3
+//		4 5 6
+//		7 8 9
+//	3. Display the numbers 1 through 100 in a 10x10 square grid
+//	4. Display the following output:
+//		*
+//		**
+//		***
+//		****
+//		*****
+//		******
+//
+//	*BONUS*
+//	1. Write a for loop that counts down from 100 to 0. However, the for loop must start with for(int i = 0;  and you may not have any code above your for loop.
+//
+//	2. 	Fizz Buzz		
+	
+}
