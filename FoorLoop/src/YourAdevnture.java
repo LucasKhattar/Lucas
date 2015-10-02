2cb54026c2e2018c6798f6829cbd0955d7185464
LucasKhattar <Guest@wts9.attlocal.net>
2015-10-01 19:54:56 -0700

    Yay…….heh

---
+++ FoorLoop/src/YourAdevnture.java
@@ -0,0 +1,52 @@
+import javax.swing.JOptionPane;
+
+
+public class YourAdevnture {
+public static void main(String[] args) {
+	String a = JOptionPane.showInputDialog(null, "Tunnel 1: Chocloate Land | Tunnel 2: Candy Land");
+	if(a.equals("joesmoe")){
+	String no = JOptionPane.showInputDialog(null, "You win!");
+	if(no.equals("no i dont")){
+		JOptionPane.showMessageDialog(null, "OR DO YOU?");
+		return;
+	}
+
+	}
+	
+
+if(a.equals("1")){
+	JOptionPane.showMessageDialog(null, "Good Job!");
+}
+else{JOptionPane.showMessageDialog(null, "You Die");
+return;}
+	String b = JOptionPane.showInputDialog(null, "Snakes 1 | Sharks 2");
+	if(b.equals("1")){
+		JOptionPane.showMessageDialog(null, "Good Job!");
+	}
+	else{JOptionPane.showMessageDialog(null, "You Die");
+	return;}
+	String c = JOptionPane.showInputDialog(null, "");		
+	String d = JOptionPane.showInputDialog(null, "");		
+	String e = JOptionPane.showInputDialog(null, "");		
+	String f = JOptionPane.showInputDialog(null, "");		
+	String g = JOptionPane.showInputDialog(null, "");		
+	String h = JOptionPane.showInputDialog(null, "");		
+	String i = JOptionPane.showInputDialog(null, "");		
+	String j = JOptionPane.showInputDialog(null, "");		
+	String k = JOptionPane.showInputDialog(null, "");		
+	String l = JOptionPane.showInputDialog(null, "");		
+	String m = JOptionPane.showInputDialog(null, "");		
+	String n = JOptionPane.showInputDialog(null, "");		
+	String o = JOptionPane.showInputDialog(null, "");		
+	String p = JOptionPane.showInputDialog(null, "");		
+	String q = JOptionPane.showInputDialog(null, "");		
+	String r = JOptionPane.showInputDialog(null, "");		
+	String s = JOptionPane.showInputDialog(null, "");		
+	String t = JOptionPane.showInputDialog(null, "");		
+	String u = JOptionPane.showInputDialog(null, "");		
+	String v = JOptionPane.showInputDialog(null, "");		
+	String w = JOptionPane.showInputDialog(null, "");		
+	String x = JOptionPane.showInputDialog(null, "");		
+	String y = JOptionPane.showInputDialog(null, "");		
+	String z = JOptionPane.showInputDialog(null, "");
+}}
